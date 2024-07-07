65e375dccbb810504de93a23d9f0673b549fe65b
Megha Arya <153416455+mayghhaa@users.noreply.github.com>
2024-07-07 12:50:57 +0530

    Final Files

--- chrome_yt_extension/app.py
+++ chrome_yt_extension/app.py
@@ -1,12 +1,15 @@
-from dotenv import load_dotenv
 from flask import Flask, request
 from youtube_transcript_api import YouTubeTranscriptApi
+from dotenv import load_dotenv
 import os
 import google.generativeai as genai
 
+
 app = Flask(__name__)
 
-load_dotenv()  # Load all the environment variables
+
+# Load environment variables
+load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 prompt = """You are a YouTube video summarizer. You will be taking the transcript text
@@ -17,25 +20,25 @@ within 500 words. Please provide the summary of the text given here: """
 def summary_api():
     url = request.args.get('url', '')
     video_id = url.split('=')[1]
-    transcript_text = extract_transcript_details(video_id)
+    transcript_text = get_transcript(video_id)
     summary = get_summary(transcript_text, prompt)
-    return summary, 200
+    return summary, 200  # Return plain text response
 
-# Getting the transcript data from YouTube videos
-def extract_transcript_details(video_id):
+def get_transcript(video_id):
     try:
-        transcript_data = YouTubeTranscriptApi.get_transcript(video_id)
-        transcript = " ".join([item["text"] for item in transcript_data])
+        transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
+        transcript = ' '.join([d['text'] for d in transcript_list])
         return transcript
     except Exception as e:
         raise e
 
-def get_summary(transcript_text, prompt):
-    model = genai.GenerativeModel("gemini-pro")
-    response = model.generate_content(prompt + transcript_text)
-    return response.text
-
-
+def get_summary(transcript, prompt):
+    try:
+        model = genai.GenerativeModel("gemini-pro")
+        response = model.generate_content(prompt + transcript)
+        return response.text
+    except Exception as e:
+        raise e
 
 if __name__ == '__main__':
     app.run()
